c153b795e8caeff00d9979f9e908c47bcc249119
Romain Gille <romain.gille@gmail.com>
2016-06-14 11:54:22 +0200

    suppr inutiles

--- scripts-shell/progressBar.sh
+++
@@ -1,9 +0,0 @@
-total=30
-pstr="[=======================================================================]"
-
-while [ $count -lt $total ]; do
-  sleep 0.5 # this is work
-  count=$(( $count + 1 ))
-  pd=$(( $count * 73 / $total ))
-  printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
-done
