25e9ad3508d9ba6048e3a167789a44b915afc9c7
Romain Gille <romain.gille@gmail.com>
2016-06-16 10:25:22 +0200

    clean code + rm useless files

--- scripts/cmpfactor.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import sys
-from fonctions import comparaisonImage3channels2
-
-ims = [Image.open(f) for f in sys.argv[1:]]
-if ims[0].mode == "RGB":
-    comparaisonImage3channels2(ims[0], ims[1], 0.5)
--- scripts/moyenneCmp.py
+++
@@ -1,8 +0,0 @@
-from cmpImages import cmpImages
-from compHistos import cmpHistos
-from cmpSobel import cmpSobel
-
-
-def moyenneCmp():
-    moyenne = (cmpImages + cmpHistos + cmpSobel) / 3
-    return moyenne
